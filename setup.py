cd83696f10148d6776a62a64b90cd9ecd1744cd9
Ivan <ivan.vershigora@gmail.com>
2014-09-16 21:09:11 +0400

    new release

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from os.path import join, dirname
 
 setup(
     name='django-cc',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT License',
     description='Django wallet for Bitcoin and other cryptocurrencies',
     author='Ivan Vershigora',
     author_email='ivan.vershigora@gmail.com',
     url='https://github.com/limpbrains/django-cc',
-    download_url = 'https://github.com/limpbrains/django-cc/tarball/0.0.1',
+    download_url = 'https://github.com/limpbrains/django-cc/tarball/0.0.2',
     keywords='bitcoin litecoin django wallet',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
