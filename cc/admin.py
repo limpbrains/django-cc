8676b1a15415dce5f71b90f080e9902f50b207c7
Ivan <ivan.vershigora@gmail.com>
2016-05-19 22:48:50 +0300

    admin.py

--- cc/admin.py
+++ cc/admin.py
@@ -8,14 +8,14 @@ from .forms import WalletAdminForm
 
 class WalletAdmin(admin.ModelAdmin):
     form = WalletAdminForm
-    list_display = ('currency', 'balance', 'holded', 'unconfirmed', 'label', 'get_address')
+    list_display = ('id', 'currency', 'balance', 'holded', 'unconfirmed', 'label', 'get_address')
     list_filter = ('currency',)
 
 admin.site.register(models.Wallet, WalletAdmin)
 
 
 class OperationAdmin(admin.ModelAdmin):
-    list_display = ('wallet', 'balance', 'holded', 'unconfirmed', 'description')
+    list_display = ('id', 'wallet', 'balance', 'holded', 'unconfirmed', 'description')
     list_filter = ('wallet',)
 
 admin.site.register(models.Operation, OperationAdmin)
@@ -28,7 +28,7 @@ admin.site.register(models.Currency, CurrencyAdmin)
 
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('txid', 'currency', 'processed')
+    list_display = ('id', 'txid', 'currency', 'processed')
     list_filter = ('currency', 'processed')
 
 admin.site.register(models.Transaction, TransactionAdmin)
@@ -42,7 +42,7 @@ admin.site.register(models.Address, AddressAdmin)
 
 
 class WithdrawTransactionAdmin(admin.ModelAdmin):
-    list_display = ('currency', 'amount', 'address', 'wallet', 'created', 'state', 'txid', 'fee')
-    list_filter = ('currency',)
+    list_display = ('id', 'currency', 'amount', 'address', 'wallet', 'created', 'state', 'txid', 'fee')
+    list_filter = ('currency', 'state')
 
 admin.site.register(models.WithdrawTransaction, WithdrawTransactionAdmin)
