c4d891529c8b8f8c368ca4517023283389d1aa8b
Ivan Vershigora <ivan.vershigora@gmail.com>
2017-01-02 20:32:09 +0300

    move mock to install_requires

--- setup.py
+++ setup.py
@@ -7,17 +7,21 @@ setup(
     author_email='ivan.vershigora@gmail.com',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
+        'Framework :: Django'
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Framework :: Django'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools',
     ],
     description='Django wallet for Bitcoin and other cryptocurrencies',
     download_url = 'https://github.com/limpbrains/django-cc/tarball/0.2',
     install_requires=[
-        'Django>=1.7',
         'celery',
+        'Django>=1.7',
+        'mock',
         'pycoin',
         'python-bitcoinrpc>=1.0',
     ],
@@ -26,9 +30,6 @@ setup(
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     name='django-cc',
     packages=find_packages(),
-    tests_require=[
-        'mock',
-    ],
     url='https://github.com/limpbrains/django-cc',
     version='0.2',
 )
